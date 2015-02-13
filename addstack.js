431b93bfa57877f38ff118d19575342fae300996
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-13 13:18:00 -0500

    stack edit exercise

---
+++ addstack.js
@@ -0,0 +1,51 @@
+
+function Stack() {
+	 this.dataStore = [];
+	  this.top = 0;
+	   this.push = push;
+	    this.pop = pop;
+	     this.peek = peek;
+	      this.length = length;
+	       this.clear = clear;
+}
+
+function push(element) {
+	 this.dataStore[this.top++] = element;
+}
+
+function pop() {
+	 return this.dataStore[--this.top];
+}
+
+function peek() {
+	 return this.dataStore[this.top-1];
+}
+
+function length() {
+	 return this.top;
+}
+
+function clear() {
+	 this.top = 0;
+	  this.dataStore.length = 0;
+}
+
+var runTot = 0;
+
+stack = new Stack();
+
+stack.push(2);
+stack.push(4);
+stack.push(6);
+stack.push(8);
+stack.push(10);
+
+var prev = stack.length();
+var runTot = 0;
+for (var i = 0; i < prev; i++) {
+	runTot += stack.pop();
+}
+print(runTot);
+
+
+
